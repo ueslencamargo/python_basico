5c59f6afdc4a54257bcd42e1026fc8808f1fe572
Ueslen Camargo <132022648+ueslencamargo@users.noreply.github.com>
2023-06-17 15:12:44 -0300

    Update multiplicando_2N.py

--- multiplicando_2N.py
+++ multiplicando_2N.py
@@ -1,1 +1,10 @@
+# Criar uma FUNÇÃO  que múltiplica 2 números inteiros:
 
+# A função tem 2 parâmetros que são a,b números inteiros.
+
+# E o retorno dela vai ter outro inteiro, no caso o resultado da multiplicação.
+
+def mult_two(a:int,b:int)->int:
+    return a*b
+
+assert mult_two(3, 2) == 6  # Aqui a função é testada com assert.
